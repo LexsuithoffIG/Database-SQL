59197bcf4e82e0dbd0ac2ffb0d2b647e8320c03a
Lex Suithoff <lex@intelligence-group.nl>
2021-09-19 23:01:51 +0200

    ETL GTAM

--- Database/Database/Currency.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Currency]
-(
-	[Currency_Code] INT NOT NULL PRIMARY KEY,
-	[Currency_Name] NVARCHAR(50) NULL,
-	[Currency_Description] NVARCHAR(50) NULL,
-	[Load_Dts] Datetime NULL,
-	[Valid] VARCHAR(1) NOT NULL
-)
