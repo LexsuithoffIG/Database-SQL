fc19c5227b2bc16a0f87ead62c94bc57f8e3994b
Lex Suithoff <lex@intelligence-group.nl>
2021-09-19 22:28:46 +0200

    Final

--- Database/Database/Currency.sql
+++ Database/Database/Currency.sql
@@ -2,5 +2,6 @@
 (
 	[Currency_Code] INT NOT NULL PRIMARY KEY,
 	[Currency_Name] NVARCHAR(50) NULL,
-	[Currency_Description] NVARCHAR(50) NULL
+	[Currency_Description] NVARCHAR(50) NULL,
+	[Load_Dts] Datetime NULL
 )
